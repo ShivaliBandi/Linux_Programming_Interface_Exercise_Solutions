2eaa099325797ae95ec7135ff1f8fa58289f1b6f
ShivaliBandi <shivbandi3004@gmail.com>
2022-07-05 14:11:43 +0530

    chapter 3 exercise solution changes for reading

--- 03_System_Programming_Concepts/exercise_03_01.c
+++ 03_System_Programming_Concepts/exercise_03_01.c
@@ -4,13 +4,20 @@
 int main(int argc,char **argv)
 {
     printf("LINUX_REBOOT_MAGIC1:%x\n",LINUX_REBOOT_MAGIC1);
-    printf("LINUX_REBOOT_MAGIC2:%d\n",LINUX_REBOOT_MAGIC2);
+    printf("LINUX_REBOOT_MAGIC2:%x\n",LINUX_REBOOT_MAGIC2);
     printf("LINUX_REBOOT_MAGIC2A:%x\n",LINUX_REBOOT_MAGIC2A);
     printf("LINUX_REBOOT_MAGIC2A:%x\n",LINUX_REBOOT_MAGIC2A);
     printf("LINUX_REBOOT_MAGIC2A:%x\n",LINUX_REBOOT_MAGIC2B);
     printf("LINUX_REBOOT_MAGIC2A:%x\n",LINUX_REBOOT_MAGIC2C);
     /*
-    
+    OUTPUT:
+    LINUX_REBOOT_MAGIC1:fee1dead
+    LINUX_REBOOT_MAGIC2:28121969
+    LINUX_REBOOT_MAGIC2A:5121996
+    LINUX_REBOOT_MAGIC2A:5121996
+    LINUX_REBOOT_MAGIC2A:16041998
+    LINUX_REBOOT_MAGIC2A:20112000
+
     The meaning of these numbers is more prosaic.
     When represented in hexadecimal numeral system, they equal 28121969, 05121996, 16041998 and 20112000 respectively;
     the first of which is the date of birth of Linus Torvalds himself, 
